a1229b0cc47b782f4cdc689e713bfd56413f0636
najalee <najaleeh@gmail.com>
2023-05-12 00:02:22 -0500

    fixed project link

--- src/pages/ProjectsScreen.js
+++ src/pages/ProjectsScreen.js
@@ -16,7 +16,7 @@ function ProjectsScreen() {
 
       <div class="projectsList">
         <ProjectPreview
-          link="https://github.com/najalee"
+          link="https://github.com/najalee/portfolio_website"
           name="portfolio_website"
           description="a personal website that displays past work and projects. created using React.js and hosted with Github Pages"
         />
